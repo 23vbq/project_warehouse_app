65c1370329a5a7b256a97e290795bb7a6eb111fb
23vbq <42277522+23vbq@users.noreply.github.com>
2026-05-04 17:34:19 +0200

    fix turbo frame and turbo form redirect

--- assets/app.js
+++ assets/app.js
@@ -4,13 +4,22 @@ import 'flowbite';
 import * as Turbo from '@hotwired/turbo';
 
 document.addEventListener('turbo:before-fetch-response', (event) => {
+    console.log('turbo:before-fetch-response', event);
     const fetchResponse = event.detail.fetchResponse;
+    console.log('fetchResponse', fetchResponse.succeeded, fetchResponse.redirected, fetchResponse.location);
 
     if (!fetchResponse.succeeded || !fetchResponse.redirected) return;
 
     const frame = document.querySelector('turbo-frame[busy]');
+    console.log('frame', frame, frame.dataset.turboFormRedirect);
     if (!frame || !frame.dataset.turboFormRedirect) return;
 
     event.preventDefault();
     Turbo.visit(fetchResponse.location);
+    console.log('Turbo.visit', fetchResponse.location);
+});
+
+document.addEventListener('turbo:frame-load', (event) => {
+    console.log('turbo:frame-load');
+    initFlowbite();
 });
