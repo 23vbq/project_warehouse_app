0c47bf74f3bdfc0d45971186c1a4ffd81ae256c1
23vbq <42277522+23vbq@users.noreply.github.com>
2026-05-13 22:21:54 +0200

    Feat/product_show (#6)
    * wip show
    
    * fix view
    
    * fix colspan
    
    * kpi
    
    * chartjs
    
    * stock trend
    
    * linter
    
    * fixes
    
    * fix
    
    * cr fix

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -136,4 +136,25 @@ class ProductRepository extends ServiceEntityRepository
         return $qb->getQuery()
             ->getResult();
     }
+
+    public function getKpiQueryBuilder(): QueryBuilder
+    {
+        return $this->createQueryBuilder('p')
+            ->select(
+                'SUM(s.quantity) as totalStock',
+                'SUM(s.quantity * p.unitPrice) as totalValue',
+                'COUNT(DISTINCT s.location) as locationCount'
+            )
+            ->leftJoin('p.stocks', 's');
+    }
+
+    public function getKpiForProduct(Product $product): array
+    {
+        return $this->getKpiQueryBuilder()
+            ->where('p.id = :productId')
+            ->setParameter('productId', $product->getId())
+            ->groupBy('p.id')
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
 }
